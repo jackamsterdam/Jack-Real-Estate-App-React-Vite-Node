80c69fd9fb48c4c419ecc291c0888fb3c58386be
Jack Amsterdam Matrix <64046793+jackamsterdam@users.noreply.github.com>
2024-05-04 23:13:16 +0300

    homepage 2 parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./layout.scss"
 import Jack from "./Jack"
 import NavBar from "./components/navbar/NavBar"
+import HomePage from "./routesorpages/homePage/homePage"
 
 function App() {
   return (
     <div className="layout">
-      <NavBar />
-    {/* <Jack/> */}
+      <div className="navbar">
+        <NavBar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+      {/* <Jack/> */}
     </div>
   )
 }
