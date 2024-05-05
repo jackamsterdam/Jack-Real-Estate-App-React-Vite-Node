b5d643349890cdbd4b08dc0b33d0849596e9ae52
Jack Amsterdam Matrix <64046793+jackamsterdam@users.noreply.github.com>
2024-05-05 14:52:06 +0300

    finished routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import "./layout.scss"
+// import "./layout.scss"
 import Jack from "./Jack"
-import NavBar from "./components/navbar/NavBar"
+// import NavBar from "./components/navbar/NavBar"
+// import HomePage from "./routesorpages/homePage/homePage"
+import { createBrowserRouter,RouterProvider} from "react-router-dom"
+import ListPage from "./routesorpages/listPage/listPage"
+import Layout from "./routesorpages/layout/layout"
 import HomePage from "./routesorpages/homePage/homePage"
+import SinglePage from "./routesorpages/singlePage/singlePage"
 
 function App() {
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage/>
+      },
+      {
+        path: '/list',
+        element: <ListPage/>
+      },
+      {
+        path: '/:id',
+        element: <SinglePage/>
+      }
+    ]
+  }
+ 
+]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <NavBar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-      {/* <Jack/> */}
-    </div>
-  )
+
+
+      <RouterProvider router={router}/>
+  );
 }
 
 export default App
+
+      
