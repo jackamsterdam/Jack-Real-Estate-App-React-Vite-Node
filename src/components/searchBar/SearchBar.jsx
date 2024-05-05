fc2966b466a8dc878761efde5917848f39c5549f
Jack Amsterdam Matrix <64046793+jackamsterdam@users.noreply.github.com>
2024-05-05 12:12:48 +0300

    finished home page

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import './searchBar.scss';
 
+const types = ["buy", "rent"]
+
 function SearchBar() {
+    const [query, setQuery] = useState({
+        type:"buy",
+        location: "",
+        minPrice: 0,
+        maxPrice: 0
+    })
+
+    const switchType = (val) => {
+       setQuery((prev) => ({...prev,type: val}))
+    }
+
+
     return (
-        <div className="searchBar">SearchBar</div>
+        <div className="searchBar">
+            <div className="type">
+                {types.map((type) => (
+                    <button key={type} onClick={() => switchType(type)} 
+                    className={query.type === type ? 'active' : ""}
+                    >
+                        {type}
+                    </button>
+                ))}
+                {/* Dont repeat yourself:  use above instead*/}
+                {/* <button onClick={() => switchType("buy")}>Buy</button> */}
+                {/* <button onClick={() => switchType("rent")}>Rent</button> */}
+            </div>
+            <form>
+                <input type="text" name="location" placeholder='City Location' />
+                <input type="number" name="minPrice" min={0} max={10000000} placeholder='Min Price' />
+                <input type="number" name="maxPrice" min={0} max={10000000} placeholder='Max Price' />
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </form>
+        </div>
     )
 }
+
 
 export default SearchBar
